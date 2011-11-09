4ed085efbb0d39998bbb858cc2e9ba6d67a0ba02
myliang <liangyuliang0335@gmail.com>
2011-11-09 16:39:31 +0800

    delete files

--- 20111103141854_change_id_integer_to_string_from_countries.rb
+++
@@ -1,8 +0,0 @@
-class ChangeCountriesColumnIdFromIntegerToString < ActiveRecord::Migration
-  def self.up
-    change_column :countries, :id, :string, :limit => 50
-  end
-  def self.down
-    change_column :countries, :id, :integer
-  end
-end
